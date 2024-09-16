bcb9d81f91e904e2871ce47f76a3ac5a9417502b
MayurK-cmd <mayurgk2006@gmail.com>
2024-09-16 17:14:47 +0530

    App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import SignupForm from './components/SignupForm.jsx';
 import LoginForm from './components/LoginForm.jsx';
 import DeleteUserForm from './components/DeleteUserForm.jsx';
 
 function App() {
   return (
-    <div>
-      <h1>Signup</h1>
-      <SignupForm />
-      <h1>Login</h1>
-      <LoginForm />
-      <h1>Delete User</h1>
-      <DeleteUserForm />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          {/* Navigation Links */}
+          <ul>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/delete-user">Delete User</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Routes */}
+        <Routes>
+          <Route path="/signup" element={<SignupForm />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/delete-user" element={<DeleteUserForm />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
